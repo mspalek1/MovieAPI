9b1e92c80ce5b18241369ae6a9edf207f29e249a
mspalek <mspalek1@gmail.com>
2022-09-13 15:34:04 +0200

    add movie controller tests

---
+++ MovieAPI.Tests/MovieControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using MovieAPI.Presentations.Controllers;
+using NUnit.Framework;
+using Services.Interfaces;
+using Services.Services;
+
+namespace MovieAPI
+{
+    [TestFixture]
+    public class MovieControllerTests
+    {
+        private Mock<IServiceManager> _serviceManager;
+        private Mock<IMovieService> _movieService;
+        private MovieController _movieController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _movieService = new Mock<IMovieService>();
+            _serviceManager = new Mock<IServiceManager>();
+            _serviceManager.Setup(s => s.MovieService).Returns(_movieService.Object);
+            
+            _movieController = new MovieController(_serviceManager.Object);
+        }
+
+        [Test]
+        public void GetAll_CallRequest_VerifyGetAllInvoked()
+        {
+            _movieController.GetAll();
+            _serviceManager.Verify(x=>x.MovieService.GetAll(), Times.Once);
+        }
+    }
+}
