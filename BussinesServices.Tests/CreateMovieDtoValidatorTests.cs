9a7705ff4b6f14be3eba795ebf00e0641907675a
mspalek <mspalek1@gmail.com>
2022-09-21 12:29:14 +0200

    add next NUnitTest

--- BussinesServices.Tests/CreateMovieDtoValidatorTests.cs
+++ BussinesServices.Tests/CreateMovieDtoValidatorTests.cs
@@ -1,36 +1,40 @@
-﻿//using Domain.Queries;
-//using FluentValidation.TestHelper;
-//using Models.Validators;
-//using NUnit.Framework;
+﻿using Domain.Repositories;
+using FluentValidation.TestHelper;
+using Moq;
+using NUnit.Framework;
+using Services;
+using Services.Function.Movie.Commands.CreateMovie;
 
-//namespace Models
-//{
-//    [TestFixture]
-//    public class CreateMovieDtoValidatorTests
-//    {
-//        private CreateMovieDtoValidator _createMovieDtoValidator;
+namespace Models
+{
+    [TestFixture]
+    public class CreateMovieDtoValidatorTests
+    {
+        private CreatedMovieCommandValidator _createMovieDtoValidator;
+        private Mock<IMovieAsyncRepository> _repositoryMovieMock;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _createMovieDtoValidator = new CreateMovieDtoValidator();
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            _repositoryMovieMock = RepositoryMock.GetMovieAsyncRepository();
+            _createMovieDtoValidator = new CreatedMovieCommandValidator(_repositoryMovieMock.Object);
+        }
 
-//        [Test]
-//        public void MovieDtoValidator_MovieDto_ShouldBeValid()
-//        {
-//            var movieDto = new CreateMovieDto();
-//            movieDto.Name = "Name";
-//            var result = _createMovieDtoValidator.TestValidate(movieDto);
-//            result.ShouldNotHaveValidationErrorFor(r => r.Name);
-//        }
+        [Test]
+        public void MovieDtoValidator_MovieDto_ShouldBeValid()
+        {
+            var movieDto = new CreatedMovieCommand();
+            movieDto.Name = "Name";
+            var result = _createMovieDtoValidator.TestValidate(movieDto);
+            result.ShouldNotHaveValidationErrorFor(r => r.Name);
+        }
 
-//        [Test]
-//        public void MovieDtoValidator_MovieDto_ShouldBeNotValid()
-//        {
-//            var movieDto = new CreateMovieDto();
-//            var result = _createMovieDtoValidator.TestValidate(movieDto);
-//            result.ShouldHaveValidationErrorFor(r => r.Name);
-//        }
-//    }
-//}
+        [Test]
+        public void MovieDtoValidator_MovieDto_ShouldBeNotValid()
+        {
+            var movieDto = new CreatedMovieCommand();
+            var result = _createMovieDtoValidator.TestValidate(movieDto);
+            result.ShouldHaveValidationErrorFor(r => r.Name);
+        }
+    }
+}
--- BussinesServices.Tests/MovieQueryValidatorTests.cs
+++ BussinesServices.Tests/MovieQueryValidatorTests.cs
@@ -1,123 +1,123 @@
-﻿//using Domain.Entities;
-//using Domain.Queries;
-//using FluentValidation.TestHelper;
-//using Models.Validators;
-//using NUnit.Framework;
-//using NUnit.Framework.Internal;
+﻿using Domain.Entities;
+using Domain.Queries;
+using FluentValidation.TestHelper;
+using Models.Validators;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
-//namespace Models
-//{
-//    [TestFixture]
-//    public class MovieQueryValidatorTests
-//    {
-//        private MovieQueryValidator _movieQueryValidator;
+namespace Models
+{
+    [TestFixture]
+    public class MovieQueryValidatorTests
+    {
+        private MovieQueryValidator _movieQueryValidator;
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _movieQueryValidator = new MovieQueryValidator();
-//        }
-
-
-//        [TestCase(1)]
-//        [TestCase(5)]
-//        [TestCase(10)]
-//        [TestCase(15)]
-//        public void MovieQueriesValidator_PageSize_ShouldBeValid(int pageSize)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.PageSize = pageSize;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldNotHaveValidationErrorFor(r=>r.PageSize);
-//        }
-
-//        [TestCase(-1)]
-//        [TestCase(0)]
-//        [TestCase(25)]
-//        [TestCase(-15)]
-//        public void MovieQueriesValidator_PageSize_ShouldBeNotValid(int pageSize)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.PageSize = pageSize;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldHaveValidationErrorFor(r => r.PageSize);
-//        }
-
-//        [TestCase(1)]
-//        [TestCase(5)]
-//        [TestCase(10)]
-//        [TestCase(15)]
-//        public void MovieQueriesValidator_PageNumber_ShouldBeValid(int pageNumber)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.PageNumber = pageNumber;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldNotHaveValidationErrorFor(r => r.PageNumber);
-//        }
-
-//        [TestCase(-1)]
-//        [TestCase(0)]
-//        [TestCase(-25)]
-//        [TestCase(-15)]
-//        public void MovieQueriesValidator_PageNumber_ShouldBeNotValid(int pageNumber)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.PageNumber = pageNumber;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldHaveValidationErrorFor(r => r.PageNumber);
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            _movieQueryValidator = new MovieQueryValidator();
+        }
 
 
-//        private readonly string[] _allowedSortByColumnNames =
-//        {
-//            nameof(Movie.Name),
-//            nameof(Movie.Description),
-//            nameof(Movie.MovieLength),
-//        };
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(15)]
+        public void MovieQueriesValidator_PageSize_ShouldBeValid(int pageSize)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.PageSize = pageSize;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldNotHaveValidationErrorFor(r => r.PageSize);
+        }
 
-//        [TestCase(nameof(Movie.Name))]
-//        [TestCase(nameof(Movie.Description))]
-//        [TestCase(nameof(Movie.MovieLength))]
-   
-//        public void MovieQueriesValidator_SortByColumn_ShouldBeValid(string columnName)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.SortBy = columnName;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldNotHaveValidationErrorFor(r => r.SortBy);
-//        }
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(25)]
+        [TestCase(-15)]
+        public void MovieQueriesValidator_PageSize_ShouldBeNotValid(int pageSize)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.PageSize = pageSize;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldHaveValidationErrorFor(r => r.PageSize);
+        }
 
-//        [TestCase(nameof(Movie.AgeCategory))]
-//        [TestCase(nameof(Movie.Price))]
-//        public void MovieQueriesValidator_SortByColumn_ShouldBeNotValid(string columnName)
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.SortBy = columnName;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            result.ShouldHaveValidationErrorFor(r => r.SortBy);
-//        }
+        [TestCase(1)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(15)]
+        public void MovieQueriesValidator_PageNumber_ShouldBeValid(int pageNumber)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.PageNumber = pageNumber;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldNotHaveValidationErrorFor(r => r.PageNumber);
+        }
 
-//        [Test]
-//        public void MovieQueriesValidator_AllQuery_ShouldBeValid()
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.SortBy = nameof(Movie.Name);
-//            movieQuery.PageNumber = 1;
-//            movieQuery.PageSize = 5;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            Assert.That(result.IsValid, Is.True);
-//        }
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(-25)]
+        [TestCase(-15)]
+        public void MovieQueriesValidator_PageNumber_ShouldBeNotValid(int pageNumber)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.PageNumber = pageNumber;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldHaveValidationErrorFor(r => r.PageNumber);
+        }
 
-//        [Test]
-//        public void MovieQueriesValidator_AllQuery_ShouldBeNotValid()
-//        {
-//            var movieQuery = new MovieQuery();
-//            movieQuery.SortBy = nameof(Movie.Name);
-//            movieQuery.PageNumber = -1;
-//            movieQuery.PageSize = 5;
-//            var result = _movieQueryValidator.TestValidate(movieQuery);
-//            Assert.That(result.IsValid, Is.False);
-//        }
 
-//    }
-//}
+        private readonly string[] _allowedSortByColumnNames =
+        {
+            nameof(Movie.Name),
+            nameof(Movie.Description),
+            nameof(Movie.MovieLength),
+        };
+
+        [TestCase(nameof(Movie.Name))]
+        [TestCase(nameof(Movie.Description))]
+        [TestCase(nameof(Movie.MovieLength))]
+
+        public void MovieQueriesValidator_SortByColumn_ShouldBeValid(string columnName)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.SortBy = columnName;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldNotHaveValidationErrorFor(r => r.SortBy);
+        }
+
+        [TestCase(nameof(Movie.AgeCategory))]
+        [TestCase(nameof(Movie.Price))]
+        public void MovieQueriesValidator_SortByColumn_ShouldBeNotValid(string columnName)
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.SortBy = columnName;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            result.ShouldHaveValidationErrorFor(r => r.SortBy);
+        }
+
+        [Test]
+        public void MovieQueriesValidator_AllQuery_ShouldBeValid()
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.SortBy = nameof(Movie.Name);
+            movieQuery.PageNumber = 1;
+            movieQuery.PageSize = 5;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            Assert.That(result.IsValid, Is.True);
+        }
+
+        [Test]
+        public void MovieQueriesValidator_AllQuery_ShouldBeNotValid()
+        {
+            var movieQuery = new MovieQuery();
+            movieQuery.SortBy = nameof(Movie.Name);
+            movieQuery.PageNumber = -1;
+            movieQuery.PageSize = 5;
+            var result = _movieQueryValidator.TestValidate(movieQuery);
+            Assert.That(result.IsValid, Is.False);
+        }
+
+    }
+}
